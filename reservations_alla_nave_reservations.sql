4387dbbcd15e84a4efc4e4f27793959a8b900ce8
Maximiliano Podesta <139072821+MaxiPodesta@users.noreply.github.com>
2023-10-30 19:03:50 -0300

    Add files via upload

---
+++ reservations_alla_nave_reservations.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: reservations_alla_nave
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `reservations`
+--
+
+DROP TABLE IF EXISTS `reservations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservations` (
+  `id_reservation` int NOT NULL AUTO_INCREMENT,
+  `confirmation` bit(1) DEFAULT NULL,
+  `party` int NOT NULL,
+  `reservation_date` date DEFAULT NULL,
+  `observations` varchar(255) DEFAULT NULL,
+  `time` varchar(255) DEFAULT NULL,
+  `name_user` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_reservation`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservations`
+--
+
+LOCK TABLES `reservations` WRITE;
+/*!40000 ALTER TABLE `reservations` DISABLE KEYS */;
+INSERT INTO `reservations` VALUES (13,_binary '',15,'2023-10-28','no','Lunch','Javier  Giovanessi'),(14,_binary '',10,'2023-10-28','no','Lunch','Javier  Giovanessi'),(15,_binary '\0',15,'2023-10-28','no','Lunch','Javier  Giovanessi'),(17,_binary '\0',4,'2023-10-28','no','Dinner','Javier  Giovanessi'),(18,_binary '\0',15,'2023-10-30','no','Lunch','Filipo Inzaghi'),(19,_binary '\0',15,'2023-10-28','no','Dinner','Filipo Inzaghi'),(20,_binary '\0',12,'2023-11-01','no','Lunch','Diego Maradona');
+/*!40000 ALTER TABLE `reservations` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-30 19:00:45
